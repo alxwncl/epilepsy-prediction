77e2a82bda905628ba20d9e9015fd5ad7eeaa2fd
Alexandre Nicolas <nicolasalexandre@gmail.com>
2025-02-24 17:30:06 +0100

    Fielder eigenvalue

--- main.py
+++ main.py
@@ -22,8 +22,8 @@ from plots import plot_seizure
 
 
 # File name
-patient = 4
-record = 5
+patient = 1
+record = 4
 patient_name, record_path = get_path(patient, record)
 dir_path = '..\\chb-mit-scalp-eeg-database-1.0.0\\'
 
@@ -64,29 +64,25 @@ for band, wpli_val in zip(bands_names, wpli_vals):
     print(f'Laplacians computed for band {band}')
 
 # Compute dynamic differences
-differences = [ponderated_difference(band_laplacians, frobenius_norm, k=50, alpha=10e-32) for band_laplacians in all_laplacians]
+# differences = [ponderated_difference(band_laplacians, frobenius_norm, k=50, alpha=10e-32) for band_laplacians in all_laplacians]
 
-# eig_results = [np.linalg.eig(laplacian) for laplacian in all_laplacians]
-# eigenvalues = np.array([result[0] for result in eig_results])
+eig_results = [np.linalg.eig(laplacian) for laplacian in all_laplacians]
+all_eigenvalues = np.array([result[0] for result in eig_results])
 # eigenvectors = np.array([result[1]*np.sign(result[1]) for result in eig_results])
-# matpos = {band: [] for band in bands_names}
-# for band_eigenvalues, band_name in zip(eigenvalues, bands_names):
-#     for i, eigenvalues in enumerate(band_eigenvalues):
-#         if np.sum(np.where(eigenvalues < 0)) > 0:
-#             matpos[band_name].append(i)
-# print(matpos)
 
 # Sort embeddings
-# sorted_indices = [np.argsort(eigenvalue) for eigenvalue in eigenvalues]
-# eigenvalues = np.array([eigenvalue[sorted_index] for eigenvalue, sorted_index in zip(eigenvalues, sorted_indices)])
+for band_eigenvalues in all_eigenvalues:
+    sorted_indices = [np.argsort(eigenvalues) for eigenvalues in band_eigenvalues]
+    band_eigenvalues = [eigenvalues[sorted_index] for eigenvalues, sorted_index in zip(band_eigenvalues, sorted_indices)]
+fielder_eigenvalues = [eigenvalue[:, 1] for eigenvalue in all_eigenvalues]
 # eigenvectors = np.array([eigenvector[:, sorted_index] for eigenvector, sorted_index in zip(eigenvectors, sorted_indices)])
 
 
 # Plot k's
 N = wpli_vals[0].shape[0]
 time_windows = window_steps(0, N, window_duration, step_duration)
-_, axes = plt.subplots(max(len(differences), 2), 1, figsize=(10, 10), sharex=True)
+_, axes = plt.subplots(max(len(fielder_eigenvalues), 2), 1, figsize=(10, 10), sharex=True)
 axes.flatten()
-for i in range(len(differences)):
-    plot_seizure(differences[i], time_windows, bands_names[i], seizures=seizures, ax=axes[i])
+for i in range(len(fielder_eigenvalues)):
+    plot_seizure(fielder_eigenvalues[i], time_windows, bands_names[i], seizures=seizures, ax=axes[i])
 plt.show()
