a87043cadfcf40cacd4d65fcff0df9b3eacda6ce
Alexandre Nicolas <nicolasalexandre@gmail.com>
2025-02-24 16:10:14 +0100

    Separate frequency bands

--- preprocessing.py
+++ preprocessing.py
@@ -1,5 +1,26 @@
+import mne
 import numpy as np
 
+
+
+def get_bands(raw, bands, dtype='array'):
+    if dtype not in ['array', 'raw']:
+        raise ValueError(f'Invalid dtype. Expected "array" or "raw". Received {dtype}.')
+    
+    # Get filtered signals
+    filtered_signals = []
+    for i in range(len(bands) - 1):
+        low = bands[i]
+        high = bands[i + 1]
+        # Create a copy of raw to avoid altering the original data
+        filtered_raw = raw.copy().filter(l_freq=low, h_freq=high, fir_design='firwin', verbose=False)
+        if dtype == 'array':
+            records, _ = filtered_raw[:]
+            filtered_signals.append(records)
+        elif dtype == 'raw':
+            filtered_signals.append(filtered_raw)
+    return filtered_signals
+        
 
 
 def differential_window(y, w=100000):
@@ -10,7 +31,6 @@ def differential_window(y, w=100000):
 
 if __name__ == '__main__':
 
-    import mne
     import matplotlib.pyplot as plt
     from plots import plot_seizure
     from util import parse_seizure_file
